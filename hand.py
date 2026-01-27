4855f4e2e88101eebd6bc05c4617682335ead247
Skyejian <sjian@pdx.edu>
2026-01-26 23:19:46 -0800

    Update hand.py
    add lable around hand, now can recognize fist, pinch , openhand now...

--- hand.py
+++ hand.py
@@ -15,25 +15,43 @@ hands = mp_hands.Hands(
 
 cap = cv2.VideoCapture(0)
 
+
+
 def recognize_gesture(landmarks):
     thumb = landmarks[4]
     index = landmarks[8]
+    Middle = landmarks[12]
+    Ring = landmarks[16]
+    Pinky = landmarks[20]
+    Rist = landmarks[0]
+    tips = [landmarks[i] for i in [8, 12, 16, 20]]
+    pips = [landmarks[i] for i in [6, 10, 14, 18]]
 
-    dist = np.linalg.norm(
+    T_I_dist = np.linalg.norm(
         np.array([thumb.x, thumb.y]) -
         np.array([index.x, index.y])
     )
-
-    if dist < 0.25:
-        return "PINCH"
+    Tip_rist_dist = np.linalg.norm(
+    np.array([tips[0].x - Rist.x, tips[0].y - Rist.y])
+)
+    Pip_rist_dist = np.linalg.norm(np.array([pips[0].x - Rist.x, pips[0].y - Rist.y])
+)
+    print("Tip_rist_dist", Tip_rist_dist)
+    print("T_I_DIDST",T_I_dist)
+    print("Pip_rist_dist", Pip_rist_dist)
+    if Tip_rist_dist < Pip_rist_dist:
+        return "Fist"
+    if T_I_dist < 0.1:
+        return "pinch"
     else:
-        return "OPEN_HAND"
+        return "Open_hand"
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-
+    frame = cv2.flip(frame, 1)
+    h, w, _ = frame.shape
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = hands.process(rgb)
 
@@ -46,6 +64,26 @@ while True:
             gesture = recognize_gesture(hand_landmarks.landmark)
 
             print(f"[GESTURE EVENT] {gesture}")
+
+        if result.multi_hand_landmarks:
+            hand = result.multi_hand_landmarks[0]
+            lm = hand.landmark
+
+        
+        xs = [p.x for p in lm]
+        ys = [p.y for p in lm]
+        x1, y1 = int(min(xs) * w), int(min(ys) * h)
+        x2, y2 = int(max(xs) * w), int(max(ys) * h)
+
+        gesture = recognize_gesture(lm)
+
+        # 
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+        # 
+        label_y = max(0, y1 - 10)
+        cv2.putText(frame, gesture, (x1, label_y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
     cv2.imshow("Hand Service", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
